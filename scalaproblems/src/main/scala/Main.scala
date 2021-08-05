70e27d9f8b3f1a4a4b5cf91d5b62b23a2b5e5df0
jay bhavsar <jay.bhavsar@laitmatus.com>
2021-08-05 11:14:57 +0530

    dropConsecutive added.

--- scalaproblems/src/main/scala/Main.scala
+++ scalaproblems/src/main/scala/Main.scala
@@ -53,8 +53,8 @@ object Main extends App {
   def findLength[A](l: List[A]): Int = {
 
     def loop[A](ls: List[A], counter: Int): Int = ls match {
-      case h :: tail => loop(tail, counter + 1)
-      case Nil => counter
+      case h :: tail      =>  loop(tail, counter + 1)
+      case Nil            =>  counter
     }
     loop(l, 0)
   }
@@ -69,8 +69,8 @@ object Main extends App {
   // Tail recursive.
   def reverseTailRecursive[A](ls: List[A]): List[A] = {
     def reverseR(result: List[A], curList: List[A]): List[A] = curList match {
-      case Nil       => result
-      case h :: tail => reverseR(h :: result, tail)
+      case Nil              =>       result
+      case h :: tail        =>       reverseR(h :: result, tail)
     }
     reverseR(Nil, ls)
   }
@@ -82,17 +82,14 @@ object Main extends App {
   def reverse[A](ls: List[A]): List[A] = {
 
     def loop[A](fstList: List[A], sndList: List[A]): List[A] = fstList match {
-      case h :: tail => loop(tail, h :: sndList)
-      case Nil => sndList
+      case h :: tail        =>  loop(tail, h :: sndList)
+      case Nil              =>  sndList
     }
 
     loop(ls, Nil)
   }
 
-  def reverseFunctional[A](ls: List[A]): List[A] = ls.foldLeft(List[A]()) {
-    (r,h) =>
-      h :: r
-  }
+  def reverseFunctional[A](ls: List[A]): List[A] = ls.foldLeft(List[A]()) ((r,h) => h :: r)
 
 //P06 (*) Find out whether a list is a palindrome.
 //  scala> isPalindrome(List(1, 2, 3, 2, 1))
@@ -110,12 +107,20 @@ object Main extends App {
 //  scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 //  res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
 
+  List('a', 'b', 'b', 'c' , 'c')
+
+  def dropConsecutive(l: List[Char]): List[Char] = l match {
+    case h  ::  s   ::  tail  if h != s    =>   h :: dropConsecutive(s :: tail)
+    case h  ::  s   ::  tail  if h == s    =>   dropConsecutive(s :: tail)
+    case h  :: Nil                         =>   h :: dropConsecutive(Nil)
+    case Nil                               =>   Nil
+  }
 
 
   def last[A](seq: Seq[A]): A = seq match {
-    case h :: Nil => h
-    case h :: tail => last(tail)
-    case _ => throw new NoSuchElementException("Not Found")
+    case h :: Nil           =>    h
+    case _ :: tail          =>    last(tail)
+    case _                  =>    throw new NoSuchElementException("Not Found")
   }
 
   def updateList[A](r: Seq[A], elementToAdd: A): Seq[A] = r.dropRight(1) :+ elementToAdd
@@ -125,18 +130,56 @@ object Main extends App {
       (myDefault, element) =>
         if(myDefault.isEmpty) myDefault :+ (1, element)
         else {
-          val (c, char_value) = last(myDefault)
+
+          val (c, char_value)   =   myDefault.last
           if(char_value == element) updateList(myDefault, (c + 1, element) )
           else myDefault :+ (1, element)
         }
     }.map{ case (count, alpha) => s"$count$alpha"}.mkString
   }
 
+  def getcount[A](l: List[A], count: Int): Int = l match {
+    case _ :: tail => getcount(tail, count + 1)
+    case Nil => count
+  }
 
+  def fixQue(x: List[Int], elm: Int, size: Int): List[Int] = (x.length, size) match {
+    case (len, sz) if len < sz      =>      x :+ elm
+    case (len, sz) if len > sz      =>      (x :+ elm).drop( (len - sz) + 1)
+    case (len, sz) if len == sz     =>       x.drop(1) :+ elm
+    case _                          =>       Nil
+  }
 
+  def fun(element: Int, ls: List[Int]): List[Int] = ls match {
+    case h :: tail  if  element > h     =>      h :: fun(element, tail)
+    case h :: tail  if  element < h     =>      element :: fun(h, tail)
+    case Nil => element :: Nil
+  }
+
+  def sorted(l: List[Int]): List[Int] = l match {
+    case h :: tail      =>    fun(h, sorted(tail))
+    case Nil            =>    Nil
+  }
+
+  sealed trait Json
+  object Json {
+    case class Str(s: String) extends Json
+    case class Num(value: Double) extends Json
+  }
+
+  //def convertToJson(x)
+  def convertToJson(x: Any): Json = x match {
+    case s: String => Json.Str(s)
+    case d: Double => Json.Num(d)
+    case i: Int => Json.Num(i.toDouble)
+  }
+  //61409
 }
 
-
+case class Person(id: Int, name: String)
+object Person {
+  def apply(name: String) : Person = new Person(1, name)
+}
 
 
 
