343cb66fd9f26c9045598430ced8aeb193216753
laitmatus <laitmatus@pop-os.localdomain>
2021-11-04 17:16:18 +0530

    utility functions added

--- scalaproblems/src/main/scala/arrayProb/ArrayTest.scala
+++ scalaproblems/src/main/scala/arrayProb/ArrayTest.scala
@@ -49,7 +49,6 @@ class ArrayTest {
     breakable {
       for(i <- 0 until ls.length; from = i + 1) {
         for(j <- from until ls.length) {
-
           if(flag) break
           else loop(i, j)
         }
@@ -57,8 +56,73 @@ class ArrayTest {
     }
   }
 
+  /*
+  * Union of two given sets is the smallest set which  contains all the
+  * elements of both the sets.
+  *
+  * A = {2,4, 5,6}
+  * B = {4, 6, 7, 8}
+  *
+  * { 2, 4, 5, 6, 7, 8 }
+  * */
+  def doUnion[A](a: List[A], b: List[A]): List[A] = {
+    def loop(f: List[A], s: List[A], result: List[A]): List[A] = (f, s) match {
+      case (Nil, Nil)	=> result
+      case (ah :: atail, Nil) if !result.contains(ah)	=> loop(atail, Nil, ah :: result)
+      case (Nil, bh :: btail) if !result.contains(bh)	=> loop(Nil, btail, bh :: result)
+      case (ah :: atail, bh :: btail)	=>
+        if(ah != bh) loop(atail, btail, ah :: bh :: result)
+        else loop(atail, btail, ah :: result)
+    }
+    loop(a, b, Nil)
+  }
 
 
+  /*
+  * Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+  *
+  * input 0101
+  * output 0011
+  *
+  *
+  * */
+
+  /*
+  * find min
+  * */
+
+  def findMin(l: List[Int]): Int = {
+    l match {
+      case f :: s :: t 	if f >= s => findMin(s :: t)
+      case f :: s :: t 	if f <= s => findMin(f :: t)
+      case f :: s :: Nil 	if f >= s => f
+      case f :: s :: Nil  if f <= s => s
+      case f :: Nil => f
+      case Nil => throw new Exception("Empty List")
+    }
+  }
+
+
+
+  /*
+  * find min max
+  * */
+
+  def loop(ls: List[Int], min: Int, max: Int): (Int, Int) = ls match {
+    case h :: t =>
+      if(h > min && h < max) loop(t, min, max)
+      else if (h > min && h > max) loop(t, min, h)
+      else if (h < min && h < max) loop(t, h, max)
+      else loop(t, min, max)
+    case Nil => (min, max)
+
+  }
+
+  def minMax(l: List[Int]): (Int, Int) = l match {
+    case f :: s :: t => loop(t, f, s)
+    case f :: Nil => throw new Exception("Invalid Input")
+    case Nil => throw new Exception("Empty list")
+  }
 
 }
 
